67fb27ca821a375e2da380cc2ae9c94fdbb4f8bd
INGENIO <ingeniokhalif@gmail.com>
2022-01-12 20:35:00 +0100

    First commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import './Button.css'
 
 const Button = ({link, imp, text}) =>{
     return(
-        <div className={`button ${imp == 'secondary' ? 'button__white' : ''}`}>
+        <div className={`button ${imp === 'secondary' ? 'button__white' : ''}`}>
             <a href={link}>
                 {text}
             </a>
