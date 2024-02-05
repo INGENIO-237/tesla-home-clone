b080f73f545f29dbd25489f7927e4224ef259c46
INGENIO <ingeniokhalif@gmail.com>
2024-02-05 17:36:36 +0100

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,92 +1,92 @@
-import './App.css';
-import Header from './components/Header'
-import Item from './components/Item'
+import "./App.css";
+import Header from "./components/Header";
+import Item from "./components/Item";
 
-import Accessories from './assets/Desktop-Accessories.jpg'
-import ModelS from './assets/Desktop-ModelS.jpeg'
-import Model3 from './assets/Desktop-Model3.jpeg'
-import ModelX from './assets/Desktop-ModelX.jpeg'
-import ModelY from './assets/Desktop-ModelY.jpeg'
-import SolarPanels from './assets/Desktop-SolarPanels.jpeg'
-import SolarRoof from './assets/Desktop-SolarRoof.jpeg'
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <div className="app__itemsContainer">
         <Item
           title="Lower Coast Solar Panels"
           desc="Money-back guarantee"
           descLink=""
-          backgroundImg={SolarPanels} 
+          backgroundImg={SolarPanels}
           leftBtnTxt="ORDER NOW"
           leftBtnLink=""
           rightBtnTxt="LEARN MORE"
           rigthBtnLink=""
-          twoButtons = 'true'
+          twoButtons="true"
           first
         />
         <Item
           title="Model S"
           desc="$69,420"
           descLink=""
-          backgroundImg={ModelS} 
+          backgroundImg={ModelS}
           leftBtnTxt="CUSTOM ORDER"
           leftBtnLink=""
           rightBtnTxt="LEARN MORE"
           rigthBtnLink=""
-          twoButtons = 'true'
+          twoButtons="true"
         />
         <Item
           title="Model 3"
           desc="Money-back guarantee"
           descLink=""
-          backgroundImg={Model3} 
+          backgroundImg={Model3}
           leftBtnTxt="CUSTPM ORDER"
           leftBtnLink=""
           rightBtnTxt="LEARN MORE"
           rigthBtnLink=""
-          twoButtons = 'true'
+          twoButtons="true"
         />
         <Item
           title="Model X"
           desc="Money-back guarantee"
           descLink=""
-          backgroundImg={ModelX} 
+          backgroundImg={ModelX}
           leftBtnTxt="CUSTOM ORDER"
           leftBtnLink=""
           rightBtnTxt="LEARN MORE"
           rigthBtnLink=""
-          twoButtons = 'true'
+          twoButtons="true"
         />
         <Item
           title="Model Y"
           desc="Money-back guarantee"
           descLink=""
-          backgroundImg={ModelY} 
+          backgroundImg={ModelY}
           leftBtnTxt="CUSTOM ORDER"
           leftBtnLink=""
           rightBtnTxt="LEARN MORE"
           rigthBtnLink=""
-          twoButtons = 'true'
+          twoButtons="true"
         />
         <Item
           title="Solar for New Roofs"
           desc="Money-back guarantee"
           descLink=""
-          backgroundImg={SolarRoof} 
+          backgroundImg={SolarRoof}
           leftBtnTxt="ORDER NOW"
           leftBtnLink=""
           rightBtnTxt="LEARN MORE"
           rigthBtnLink=""
-          twoButtons = 'true'
+          twoButtons="true"
         />
         <Item
           title="Accessories"
           desc="Money-back guarantee"
           descLink=""
-          backgroundImg={Accessories} 
+          backgroundImg={Accessories}
           leftBtnTxt="ORDER NOW"
           leftBtnLink=""
           rightBtnTxt="LEARN MORE"
@@ -98,4 +98,3 @@ function App() {
 }
 
 export default App;
- 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import './Button.css'
+import React from "react";
+import "./Button.css";
 
-const Button = ({link, imp, text}) =>{
-    return(
-        <div className={`button ${imp === 'secondary' ? 'button__white' : ''}`}>
-            <a href={link}>
-                {text}
-            </a>
-        </div>
-    )
-}
+const Button = ({ link, imp, text }) => {
+  return (
+    <div className={`button ${imp === "secondary" ? "button__white" : ""}`}>
+      <a href={link}>{text}</a>
+    </div>
+  );
+};
 
-export default Button
+export default Button;
